1709fce72ad30e6e43b635175cc5359d4c0eb651
rxsinx <raghunath.nayak.pgd2020@iimu.ac.in>
2026-04-30 17:38:12 +0530

    Update cointegration.py

--- src/cointegration.py
+++ src/cointegration.py
@@ -1,27 +1,56 @@
-from statsmodels.tsa.stattools import coint, adfuller
 import numpy as np
+from statsmodels.tsa.stattools import coint
+from statsmodels.api import OLS
 
-def test_cointegration(price_a, price_b, window):
-    # Use log prices
-    log_a = np.log(price_a)
-    log_b = np.log(price_b)
-    # Rolling cointegration? Typically done on a fixed window.
-    # Here we test each new bar with the last 'window' observations.
-    # Simplified: return whether p-value < 0.05
-    _, pvalue, _ = coint(log_a[-window:], log_b[-window:])
-    return pvalue < 0.05
+def pair_coint_metrics(price_a, price_b, window):
+    """Return (is_cointegrated, half_life, latest_correlation) for a pair."""
+    log_a = np.log(price_a.dropna())
+    log_b = np.log(price_b.dropna())
+    # Align lengths
+    min_len = min(len(log_a), len(log_b))
+    log_a = log_a.iloc[-min_len:]
+    log_b = log_b.iloc[-min_len:]
 
-def half_life(spread):
-    """Return half-life in bars from AR(1) coefficient"""
-    spread_lag = spread.shift(1)
-    spread_diff = spread - spread_lag
-    spread_lag = spread_lag[1:]
-    spread_diff = spread_diff[1:]
-    # OLS: spread_diff = alpha + phi * spread_lag
-    from statsmodels.api import OLS
-    model = OLS(spread_diff, spread_lag)
-    res = model.fit()
-    phi = res.params[0]
-    if phi < -0.01:
-        return -np.log(2) / np.log(1 + phi)
-    return None
+    if len(log_a) < window:
+        return False, None, np.nan
+
+    # Cointegration test (p-value < 0.05)
+    try:
+        _, pvalue, _ = coint(log_a.iloc[-window:], log_b.iloc[-window:])
+        is_coint = pvalue < 0.05
+    except:
+        is_coint = False
+
+    # Half-life via residual AR(1)
+    # 1) OLS: log_a = alpha + beta * log_b  on the window
+    X = log_b.iloc[-window:].values.reshape(-1, 1)
+    y = log_a.iloc[-window:].values
+    try:
+        model = OLS(y, X)
+        res = model.fit()
+        beta = res.params[0]
+        alpha = res.params[0] if len(res.params) > 1 else 0  # simplified
+        spread = log_a.iloc[-window:] - beta * log_b.iloc[-window:]
+    except:
+        spread = log_a.iloc[-window:] - log_b.iloc[-window:]
+
+    # 2) AR(1) on spread differences
+    spread_lag = spread.shift(1).dropna()
+    spread_diff = spread.diff().dropna()
+    aligned = pd.concat([spread_lag, spread_diff], axis=1).dropna()
+    if len(aligned) < 5:
+        return is_coint, None, np.nan
+
+    X_ar = aligned.iloc[:, 0].values.reshape(-1, 1)
+    y_ar = aligned.iloc[:, 1].values
+    try:
+        model_ar = OLS(y_ar, X_ar)
+        res_ar = model_ar.fit()
+        phi = res_ar.params[0]
+        half_life = -np.log(2) / np.log(1 + phi) if (1 + phi) > 0 else None
+    except:
+        half_life = None
+
+    # Latest correlation
+    corr = log_a.iloc[-window:].corr(log_b.iloc[-window:])
+    return is_coint, half_life, corr
